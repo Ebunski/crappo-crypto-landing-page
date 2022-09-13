c2f22d881818642b6970814441b5bed184e3f45f
Daniel <danielduyilemi@gmail.com>
2022-09-13 22:13:32 +0100

    more styles

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,13 @@ const Hero = () => {
 	return (
 		<div className="hero">
 			<div className="words">
-				<div className="before-h1"></div>
-				<h1>Fastest &deg; secure platform to trade crypto</h1>
+				<div className="info">
+					<div className="white">75% SAVE</div>
+					<div>For the black friday weekend</div>
+				</div>
+				<h1>Fastest & secure platform to invest in crypto</h1>
+        <p>Buy and sell cryptocurrencies rom 10M wallets with over $30 billion in transactions</p>
+
 			</div>
 			<div className="image">
 				<img src={heroImage} alt="" />
