250f15138ab8afb8f40c4a3ebc81b06ed5c41897
Daniel <danielduyilemi@gmail.com>
2022-09-13 21:36:59 +0100

    Add empty div

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import heroImage from '../assets/Illustration.svg'
+import React from 'react';
+import heroImage from '../assets/Illustration.svg';
 const Hero = () => {
-  return (
-    <div className="hero"><div className='words'><h1>Fastest &deg; secure platform to trade crypto</h1></div><div className='image'><img src={heroImage} alt="" /></div></div>
-  )
-}
+	return (
+		<div className="hero">
+			<div className="words">
+				<div className="before-h1"></div>
+				<h1>Fastest &deg; secure platform to trade crypto</h1>
+			</div>
+			<div className="image">
+				<img src={heroImage} alt="" />
+			</div>
+		</div>
+	);
+};
 
-export default Hero
+export default Hero;
