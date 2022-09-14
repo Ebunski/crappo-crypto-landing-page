8115c59579a963e84a1128231134a9791125d3d1
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-09-14 10:41:25 +0100

    resolved hero conflicts

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,14 +1,26 @@
 import React from "react";
 import heroImage from "../assets/Illustration.svg";
+import heroBar1 from "../assets/hero-bar-1.png";
+import heroBar2 from "../assets/hero-bar-2.png";
 const Hero = () => {
   return (
     <div className="hero">
       <div className="words">
-        <h1>Fastest &deg; secure platform to trade crypto</h1>
+        <div className="info">
+          <div className="white">75% SAVE</div>
+          <div>For the black friday weekend</div>
+        </div>
+        <h1>Fastest & secure platform to invest in crypto</h1>
+        <p>
+          Buy and sell cryptocurrencies rom 10M wallets with over $30 billion in
+          transactions
+        </p>
       </div>
       <div className="image">
         <img src={heroImage} alt="" />
       </div>
+      <img src={heroBar1} alt="" className="hero-bar-1" />
+      <img src={heroBar2} alt="" className="hero-bar-2" />
     </div>
   );
 };
