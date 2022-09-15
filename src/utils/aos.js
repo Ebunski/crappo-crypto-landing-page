f21a315f157d95b6b6ab55324a17c37560597197
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-09-16 00:36:33 +0100

    aos adjustment

--- src/utils/aos.js
+++ src/utils/aos.js
@@ -17,11 +17,11 @@ export function useScrollAnimation() {
 
       // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
       offset: 100, // offset (in px) from the original trigger point
-      delay: 50, // values from 0 to 3000, with step 50ms
+      delay: 2000, // values from 0 to 3000, with step 50ms
       duration: 3000, // values from 0 to 3000, with step 50ms
       easing: "ease-in-out", // default easing for AOS animations
-      once: true, // whether animation should happen only once - while scrolling down
-      mirror: false, // whether elements should animate out while scrolling past them
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: true, // whether elements should animate out while scrolling past them
       anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
     });
     AOS.refresh();
