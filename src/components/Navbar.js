5738a36724b079c63068fb08dd3086a4a06b11b3
Oyebola Ebunoluwa <oyebolaebun07@gmail.com>
2022-09-16 00:01:05 +0100

    nav erros

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,44 +1,28 @@
 import React from "react";
-import {FaBars} from "react-icons/fa";
-import {FaXMark} from "react-icons/fa";
+import { FaBars } from "react-icons/fa";
+import { FaXMark } from "react-icons/fa";
 import logo from "../assets/Logo.png";
 
 export default function Navbar() {
   return (
-		<nav>
-			<div className="brand">
-				<img src={logo} alt="Logo" />
-			</div>
-			<ul>
-				<li>Products</li>
-				<li>Features</li>
-				<li>About</li>
-				<li>Contact</li>
-				<div>
-					<div className="login">Login</div>
-					<hr />
-					<button className="button btn-call">Register</button>
-				</div>
-			</ul>
-			<div className="icon-bars">
-				< FaBars />
-			</div>
-			{/*<div className="mobilenav">
-				<div className="icon">
-					<FaBars />
-				</div>
-				<ul>
-					<li>Products</li>
-					<li>Features</li>
-					<li>About</li>
-					<li>Contact</li>
-					<div>
-						<div className="login">Login</div>
-						<hr />
-						<button className="button btn-call">Register</button>
-					</div>
-			</ul>
-			</div>*/}
-		</nav>
-	);
+    <nav>
+      <div className="brand">
+        <img src={logo} alt="Logo" />
+      </div>
+      <ul>
+        <li>Products</li>
+        <li>Features</li>
+        <li>About</li>
+        <li>Contact</li>
+        <div>
+          <div className="login">Login</div>
+          <hr />
+          <button className="button btn-call">Register</button>
+        </div>
+      </ul>
+      <div className="icon-bars">
+        <FaBars />
+      </div>
+    </nav>
+  );
 }
